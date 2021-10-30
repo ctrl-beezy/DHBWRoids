b2dda66c6f3a1a5b536368427d9c429f6f0d3f39
Julius Fischer <juliusfischer18@gmail.com>
2021-10-30 12:07:49 +0200

    Unterordner Assets erstellt und in GameObjects.h eingefügt

--- Beispielprojekt/GameObjects.h
+++ Beispielprojekt/GameObjects.h
@@ -15,13 +15,19 @@ class GameObject
 class PlayerCharacter : public GameObject
 {
 	Gosu::Sample beep, lose, earn, win;
+	Gosu::Image Rakete1, Rakete2, Rakete3, Rakete4;
 
 public:
 	Player()
-		: beep(Gosu::resource_prefix() + "Assets/Laser-Sound.wav"),
-		lose(Gosu::resource_prefix() + "Assets/Lose-Sound.wav"),
-		earn(Gosu::resource_prefix() + "Assets/Earn-Sound.wav"),
-		win(Gosu::resource_prefix() + "Assets/Winning-Sound.wav");
+		: beep(Gosu::resource_prefix() + "Assets/Sounds/Laser-Sound.wav"),
+		lose(Gosu::resource_prefix() + "Assets/Sounds/Lose-Sound.wav"),
+		earn(Gosu::resource_prefix() + "Assets/Sounds/Earn-Sound.wav"),
+		win(Gosu::resource_prefix() + "Assets/Sounds/Winning-Sound.wav");
+
+		Rakete1("Assets/Bilder/Rakete1.png"), 
+		Rakete2("Assets/Bilder/Rakete2.png"), 
+		Rakete3("Assets/Bilder/Rakete3.png"), 
+		Rakete4("Assets/Bilder/Rakete4.png");
 };
 
 class Projectile : public GameObject
@@ -40,8 +46,8 @@ class Background_music
 
 public:
 	Sound()
-		: background1(Gosu : resource_prefix() + "Assets/Background-Space1.wav"),
-		background2(Gosu : resource_prefix() + "Assets/Background-Space2.wav"),
-		background3(Gosu : resource_prefix() + "Assets/Background-Space3.wav");
+		: background1(Gosu : resource_prefix() + "Assets/Sounds/Background-Space1.wav"),
+		background2(Gosu : resource_prefix() + "Assets/Sounds/Background-Space2.wav"),
+		background3(Gosu : resource_prefix() + "Assets/Sounds/Background-Space3.wav");
 
 };
