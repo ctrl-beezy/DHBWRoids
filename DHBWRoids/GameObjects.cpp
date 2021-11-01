509a2b47d1cf604a5f51bc7a3ee3b21b9d88c4f0
ctrl-beezy <matze.ob@gmail.com>
2021-11-01 17:54:16 +0100

    header und cpp von gameobjects getrennt

--- DHBWRoids/GameObjects.cpp
+++ DHBWRoids/GameObjects.cpp
@@ -1,1 +1,75 @@
 #include "GameObjects.h"
+#include <string>
+#include <Gosu/Gosu.hpp>
+#include <cmath>
+#include <cstdint>
+
+
+void GameObject::warp(double x, double y)
+{
+    pos_x = x;
+    pos_y = y;
+}
+void GameObject::draw() const
+{
+    image.draw_rot(pos_x, pos_y, Z_OBJECTS, angle, 0.4, 0.5, 1, 1);
+}
+
+void GameObject::move()
+{
+    pos_x = Gosu::wrap(pos_x + vel_x, 0.0, double(WINDOWWIDTH));
+    pos_y = Gosu::wrap(pos_y + vel_y, 0.0, double(WINDOWHEIGHT));
+
+    vel_x *= drag;
+    vel_y *= drag;
+}
+
+void Player::turn_left()
+{
+    angle -= 5;
+}
+
+void Player::turn_right()
+{
+    angle += 5;
+}
+
+void Player::accelerate()
+{
+    vel_x += Gosu::offset_x(angle, accel);
+    vel_y += Gosu::offset_y(angle, accel);
+}
+
+void Player::deaccelerate()
+{
+    vel_x -= Gosu::offset_x(angle, accel);
+    vel_y -= Gosu::offset_y(angle, accel);
+}
+void Player::move()
+{
+    pos_x = Gosu::wrap(pos_x + vel_x, 0.0, double(WINDOWWIDTH));
+    pos_y = Gosu::wrap(pos_y + vel_y, 0.0, double(WINDOWHEIGHT));
+
+    vel_x *= drag;
+    vel_y *= drag;
+}
+
+
+void Projectile::move()
+{
+    pos_x += 25 * sin(Gosu::degrees_to_radians(angle));
+    pos_y -= 25 * cos(Gosu::degrees_to_radians(angle));
+}
+void Projectile::draw() const
+{
+    image.draw_rot(pos_x, pos_y, Z_OBJECTS, angle + 90, 1, 0.63, 2.5, 2);
+}
+
+void Asteroid::draw() const
+{
+    image.draw_rot(pos_x, pos_y, Z_OBJECTS, angle, 0.5, 0.5, size, size);
+}
+
+bool Asteroid::got_hit(const double object_pos_x,const double object_pos_y) {
+    return Gosu::distance(pos_x, pos_y, object_pos_x, object_pos_y) < sqrt(image.width() / 1.2 * image.width() / 1.2 + image.height() / 1.2 * image.height() / 1.2);
+}
--- DHBWRoids/GameObjects.h
+++ DHBWRoids/GameObjects.h
@@ -28,40 +28,27 @@ enum AsteroidSize {
 class GameObject
 {
 public:
+//properties
     Gosu::Image image;
-
     double pos_x, pos_y, vel_x, vel_y, angle , drag;
-
+    
     GameObject(std::string filename) : image(filename)
     {
         pos_x = pos_y = vel_x = vel_y = angle = 0;
         drag = 1;
     }
 
-    void warp(double x, double y)
-    {
-        pos_x = x;
-        pos_y = y;
-    }
-    void draw() const
-    {
-        image.draw_rot(pos_x, pos_y, Z_OBJECTS, angle, 0.4, 0.5, 1, 1);
-    }
-
-    void move()
-    {
-        pos_x = Gosu::wrap(pos_x + vel_x, 0.0, double(WINDOWWIDTH));
-        pos_y = Gosu::wrap(pos_y + vel_y, 0.0, double(WINDOWHEIGHT));
-
-        vel_x *= drag;
-        vel_y *= drag;
-    }
+    //methods
+    void warp(double x, double y);
+    void draw() const;
+    void move();
 };
 
 // class for player character
 class Player : public GameObject {
 
 public:
+    //properties
     uint32_t score;
     uint16_t lives;
     double drag, accel;
@@ -77,39 +64,17 @@ public:
         reload_time = 0;
         lives = 3;
     }
-    void turn_left()
-    {
-        angle -= 5;
-    }
-
-    void turn_right()
-    {
-        angle += 5;
-    }
-
-    void accelerate()
-    {
-        vel_x += Gosu::offset_x(angle, accel);
-        vel_y += Gosu::offset_y(angle, accel);
-    }
-
-    void deaccelerate()
-    {
-        vel_x -= Gosu::offset_x(angle, accel);
-        vel_y -= Gosu::offset_y(angle, accel);
-    }
-    void move()
-    {
-        pos_x = Gosu::wrap(pos_x + vel_x, 0.0, double(WINDOWWIDTH));
-        pos_y = Gosu::wrap(pos_y + vel_y, 0.0, double(WINDOWHEIGHT));
-
-        vel_x *= drag;
-        vel_y *= drag;
-    }
+    //methods
+    void turn_left();
+    void turn_right();
+    void accelerate();
+    void deaccelerate();
+    void move();
 };
 // class for projectiles
 class Projectile : public GameObject {
 public:
+    
     Projectile(double x = 0.0, double y = 0.0, double v_x = 0.0, double v_y = 0.0, double a = 0.0, std::string filename = "Assets/Bilder/bullet.png") : GameObject(filename)
     {
         pos_x = x;
@@ -118,21 +83,18 @@ public:
         vel_y = v_y;
         angle = a;
     }
-    void move()
-    {
-        pos_x += 25*sin(Gosu::degrees_to_radians(angle));
-        pos_y -= 25*cos(Gosu::degrees_to_radians(angle));
-    }
-    void draw() const
-    {
-        image.draw_rot(pos_x, pos_y, Z_OBJECTS, angle + 90, 1, 0.63, 2.5, 2);
-    }
+    
+    //methods
+    void move();
+    void draw() const;
 };
 
 // class for asteroids
 class Asteroid : public GameObject {
 public:
+    //properties
     uint16_t size;
+    
     Asteroid(double x = 0.0, double y = 0.0, double v_x = 0.0, double v_y = 0.0, double a = 0.0, std::string filename = "Assets/Bilder/asteroid.png", AsteroidSize s = big) : GameObject(filename)
     {
         pos_x = x;
@@ -142,12 +104,8 @@ public:
         angle = a;
         size = s;
     }
-    void draw() const
-    {
-        image.draw_rot(pos_x, pos_y, Z_OBJECTS, angle, 0.5, 0.5, size, size);
-    }
-
-    bool got_hit(double object_pos_x, double object_pos_y) {
-        return Gosu::distance(pos_x, pos_y, object_pos_x, object_pos_y) < sqrt(image.width()/1.2 * image.width()/1.2 + image.height()/1.2 * image.height()/1.2);
-    }
+    
+    //methods
+    void draw() const;
+    bool got_hit(const double object_pos_x, const double object_pos_y);
 };
